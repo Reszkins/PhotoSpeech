f2be8324bf8bd6dd307bc74caa9b345cfb47b01c
Oskar Reszka <oskarreszka12@gmail.com>
2023-01-09 20:36:00 +0100

    detailed errors

--- PhotoSpeech/PhotoSpeech/Program.cs
+++ PhotoSpeech/PhotoSpeech/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
 
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<LoggedUserProvider>();
